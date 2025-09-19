9cb4b8d5bc10ec7993b2fea920ddcaf7c69ba098
parchii <wainggan@gmail.com>
2025-09-18 22:36:12 -0700

    you know what fuck you make a good seed yourself

--- src/rng.rs
+++ src/rng.rs
@@ -2,36 +2,23 @@
 [Wichmann-Hill](https://en.wikipedia.org/wiki/Wichmann%E2%80%93Hill) psuedo-rng.
 */
 
-
-fn util_gen_time() -> u64 {
-	std::time::SystemTime::now()
-		.duration_since(std::time::UNIX_EPOCH)
-		.unwrap_or(std::time::Duration::from_millis(0x6969696969696969))
-		.as_millis()
-		.wrapping_pow(7)
-		.wrapping_pow(5) as u64
-}
-
 pub struct WichHill {
 	seed0: u32,
 	seed1: u32,
 	seed2: u32,
 }
 impl WichHill {
-	pub fn new(seed: u32) -> Self {
+	pub fn new_raw(seed0: u32, seed1: u32, seed2: u32) -> Self {
 		Self {
-			seed0: seed.max(1),
-			seed1: seed + 1,
-			seed2: seed + 2,
+			seed0: seed0.max(1),
+			seed1: seed1.max(1),
+			seed2: seed2.max(1),
 		}
 	}
 
-	pub fn new_entropy() -> Option<Self> {
-		getrandom::u32().ok().map(|x| WichHill::new(x))
-	}
-
-	pub fn new_time() -> Self {
-		WichHill::new(util_gen_time() as _)
+	pub fn new(seed: u32) -> Self {
+		let mut rng = XorShift32::new(seed);
+		Self::new_raw(rng.nextu(), rng.nextu(), rng.nextu())
 	}
 
 	pub fn next(&mut self) -> f64 {
@@ -47,34 +34,51 @@ impl WichHill {
 	}
 }
 
+pub struct XorShift32(u32);
+impl XorShift32 {
+	pub fn new(seed: u32) -> Self {
+		Self(seed)
+	}
+
+	pub fn nextu(&mut self) -> u32 {
+		let mut x = self.0;
+		x ^= x << 13;
+		x ^= x >> 17;
+		x ^= x << 5;
+		self.0 = x;
+		x
+	}
+
+	pub fn nextf(&mut self) -> f64 {
+		self.nextu() as f64 / u32::MAX as f64
+	}
+
+	pub fn range(&mut self, x0: f64, x1: f64) -> f64 {
+		x0 + self.nextf() * (x1 - x0)
+	}
+}
 
 pub struct XorShift64(u64);
 impl XorShift64 {
 	pub fn new(seed: u64) -> Self {
 		Self(seed)
 	}
-	
-	pub fn new_entropy() -> Option<Self> {
-		getrandom::u64().ok().map(|x| XorShift64(x))
-	}
-	
-	pub fn new_time() -> Self {
-		Self(util_gen_time() as _)
-	}
 
-	pub fn next(&mut self) -> f64 {
+	pub fn nextu(&mut self) -> u64 {
 		let mut x = self.0;
 		x ^= x << 13;
 		x ^= x >> 7;
 		x ^= x << 17;
 		self.0 = x;
-		return x as f64 / u64::MAX as f64;
+		x
+	}
+
+	pub fn nextf(&mut self) -> f64 {
+		self.nextu() as f64 / u64::MAX as f64
 	}
 
 	pub fn range(&mut self, x0: f64, x1: f64) -> f64 {
-		x0 + self.next() * (x1 - x0)
+		x0 + self.nextf() * (x1 - x0)
 	}
 }
-
-
 
